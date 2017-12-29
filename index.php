e0a6cf9b3268d26bcca2ea86022b4e82be5b111f
Ricardo Jonadabs César <pexinho@uol.com.br>
2017-12-29 14:51:41 -0200

    Directory Structure

--- index.php
+++ index.php
@@ -1,32 +1,3 @@
-<?php
-error_reporting(E_ALL & ~ E_NOTICE ); //& ~ E_DEPRECATED
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-@require_once("vendor/autoload.php");
-@require_once("project/entity.php");
-
-use project\database\ENTITY;
-
-$pessoa = ENTITY::instance("PESSOA");
-$pessoa->insert(array("nm"=>"teste","email"=>"teste@teste.com.br"));
-$id = $pessoa->Insert_ID();
-
-echo "<br/>$id";
-$pessoa->delete($id-4);
-
-echo "<br/>";
-$u = $id-1;
-$pessoa->UPDATE(array("nm" => "Teste$u", "email"=>"teste$u@teste.com.br"), $u);
-
-echo "<br/>";
-$result = $pessoa->select();
-foreach($result as $k => $f){
-	print_r($f);
-	echo "<br/>";
-}
-
-exit;
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
